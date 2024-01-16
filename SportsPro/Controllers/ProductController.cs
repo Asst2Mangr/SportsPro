97ffae98a9510ff3fee2e3bcd35767607f7b05da
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2024-01-16 12:18:50 -0600

    Added Product controller started Add/Edit page list page

---
+++ SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Collections.Generic;
+
+namespace SportsPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            List<Product> products;
+            products = new List<Product>();
+
+            return View(products);
+        }
+    }
+}
