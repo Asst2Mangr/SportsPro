5a4a3659ddb0840f902c3b416a16a01f5994d361
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2024-01-16 15:35:16 -0600

    Fixing database error

--- SportsPro/Controllers/ProductController.cs
+++ SportsPro/Controllers/ProductController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsPro.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsPro.Controllers
 {
     public class ProductController : Controller
     {
+        private SportsProContext context;
+
+        public ProductController(SportsProContext ctx)
+        {
+            context = ctx;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,6 +21,9 @@ namespace SportsPro.Controllers
         {
             List<Product> products;
             products = new List<Product>();
+
+            products = context.Product
+            .OrderBy(p => p.ProductID).ToList();
 
             return View(products);
         }
