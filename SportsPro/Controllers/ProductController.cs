30bc4f4f6f0df7fec525a356ffec165dfe2ca62b
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2024-01-17 09:30:07 -0600

    Finished with the add, edit, and delete button

--- SportsPro/Controllers/ProductController.cs
+++ SportsPro/Controllers/ProductController.cs
@@ -25,5 +25,58 @@ namespace SportsPro.Controllers
 
             return View(products);
         }
+
+        //add method allowing user to add deck to the database
+        [HttpGet]
+        public ViewResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Product = context.Products.OrderBy(p => p.Name).ToList();
+            return View("Edit", new Product());
+        }
+
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Product = context.Products.OrderBy(p => p.ProductID).ToList();
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ProductID == 0)
+                    context.Products.Add(product);
+                else
+                    context.Products.Update(product);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (product.ProductID == 0) ? "Add" : "Edit";
+                ViewBag.Decks = context.Products.OrderBy(p => p.ProductID).ToList();
+                return View(product);
+            }
+        }
+        //delete methods allowing user to delete decks 
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
