695b87a08cebad994ecb84a2c2536a0a50bd3db0
Logan Miller <logan.miller@southeasttech.edu>
2024-01-21 13:14:02 -0600

    Created add actions for Incident edit view.

--- SportsPro/Controllers/IncidentController.cs
+++ SportsPro/Controllers/IncidentController.cs
@@ -25,5 +25,43 @@ namespace SportsPro.Controllers
             var incidents = ctx.Incidents.Include(i => i.Customer).Include(i => i.Product).ToList();
             return View(incidents);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var customers = ctx.Customers.ToList().OrderBy(c => c.FullName).ToList();
+            var products = ctx.Products.ToList().OrderBy(p => p.Name).ToList();
+            var technicians = ctx.Technicians.ToList().OrderBy(t => t.Name).ToList();
+
+            ViewBag.Action = "Add";
+            ViewBag.Customers = customers;
+            ViewBag.Products = products;
+            ViewBag.Technicians = technicians;
+
+            return View("IncidentEdit", new Incident());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                ctx.Incidents.Add(incident);
+                ctx.SaveChanges();
+                return RedirectToAction("IncidentList");
+            }
+
+            var customers = ctx.Customers.ToList().OrderBy(c => c.FullName).ToList();
+            var products = ctx.Products.ToList().OrderBy(p => p.Name).ToList();
+            var technicians = ctx.Technicians.ToList().OrderBy(t => t.Name).ToList();
+
+            ViewBag.Action = "Add";
+            ViewBag.Customers = customers;
+            ViewBag.Products = products;
+            ViewBag.Technicians = technicians;
+
+            return View("IncidentEdit", incident);
+        }
+
     }
 }
