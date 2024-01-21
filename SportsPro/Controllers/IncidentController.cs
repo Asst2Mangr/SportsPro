12c676b2d857417738e81f76fddc8e04a804ad1b
Logan Miller <logan.miller@southeasttech.edu>
2024-01-21 12:15:28 -0600

    Created Incident Controller. Added list action. Created Incident view folder with all views. Updated links to direct to correct page.

---
+++ SportsPro/Controllers/IncidentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class IncidentController : Controller
+    {
+        private readonly SportsProContext ctx;
+
+        public IncidentController(SportsProContext context)
+        {
+            ctx = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IncidentList()
+        {
+            var incidents = ctx.Incidents.ToList();
+            return View(incidents);
+        }
+    }
+}
